a7c18cfa1629377e4439f415a7d1030db3b14815
Pham Cong Dinh <congdinh2395@gmail.com>
2021-04-12 20:01:13 +0700

    Complete CRUD functions and some navigate link in article module

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,4 +19,23 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end
