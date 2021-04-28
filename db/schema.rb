d54d73916f318def1b6916bdfe5584b26015bf1e
Pham Cong Dinh <congdinh2395@gmail.com>
2021-04-28 21:26:20 +0700

    Migrate user table and add FE, BE logic for user sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_025439) do
+ActiveRecord::Schema.define(version: 2021_04_28_140920) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_04_18_025439) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
