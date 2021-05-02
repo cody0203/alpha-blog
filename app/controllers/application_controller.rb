0cc5b77718ad0bd7e49929edb5e1a4852e6fbaa5
Pham Cong Dinh <congdinh2395@gmail.com>
2021-05-02 09:30:18 +0700

    Make controller method as helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
   def pagination(data)
     data.paginate(page: params[:page], per_page: 10)
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
   end
 end
